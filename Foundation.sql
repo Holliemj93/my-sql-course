46ee2f5016d27e7782532e8227f4e17cec7159dc
Holliemj93 <hollie.joyce@covwarkpt.nhs.uk>
2025-04-30 15:09:26 +0000

    END OF COURSE

--- Foundation.sql
+++ Foundation.sql
@@ -13,9 +13,34 @@ SELECT
 FROM PatientStay ps
 WHERE ps.Hospital IN ('PRUH', 'OXLEAS')
 AND ps.ward LIKE '%Surgery'
-AND ps.AdmittedDate BETWEEN '2024-02-27'AND '2024-03-01'
---AND ps.AdmittedDate >= '2024-02-27' 
---AND ps.AdmittedDate <= '2024-03-01'
+--AND ps.AdmittedDate BETWEEN '2024-02-27'AND '2024-03-01'
+ORDER BY ps.AdmittedDate DESC, ps.PatientId DESC
 
 
- 
+
+
+
+SELECT
+ps.Hospital
+,ps.Ward
+, COUNT (*) as numberofpatients
+,SUM(ps.Tariff) as TotalTariff
+,AVG(ps.Tariff) as AverageTariff
+,MIN(ps.Tariff) as MinTariff
+,MAX(ps.Tariff) as MaxTariff
+FROM PatientStay ps
+GROUP BY ps.Hospital,ps.Ward
+--ORDER BY ps.Hospital, ps.Ward
+ORDER BY Numberofpatients desc
+
+    
+SELECT 
+ps.PatientId
+, ps.AdmittedDate
+, dh.HospitalType
+, dh.HospitalSize 
+FROM PatientStay ps
+INNER JOIN DimHospitalBad dh ON ps.Hospital = dh.Hospital
+WHERE dh.Hospital IS NULL
+
+
